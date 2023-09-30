25e87aaa58997d7a8df0391c8f3ffa1101f36b6a
John komba <john.mwangi1@student.moringaschool.com>
2023-09-30 10:37:35 +0300

    done w assignment

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function fetchBooks() {
+
+function fetchBooks() {fetch('https://anapioficeandfire.com/api/books')
+.then(resp => resp.json()) // Convert the response to JSON
+.then(json => renderBooks(json));
   // To pass the tests, don't forget to return your fetch!
   
 }
@@ -15,3 +18,28 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+//fetch("https://anapioficeandfire.com/api/books")
+  //.then((resp) => resp.json())
+  //.then((books) => {
+    // Finding the 5th book in the series
+    //const fifthBook = books[4];
+    //console.log("The 5th book in the series:", fifthBook.name);
+
+    // Finding the 1031st character in the series
+    //fetch("https://anapioficeandfire.com/api/characters/1031")
+      //.then((resp) => resp.json())
+      //.then((character) => {
+     //   console.log("The 1031st character in the series:", character.name);
+      //});
+
+    // Calculating the total number of pages of all the books
+    //const totalNumberOfPages = books.reduce((total, book) => total + book.numberOfPages, 0);
+    //console.log("The total number of pages of all the books:", totalNumberOfPages);
+  //});
+
+
+
+
+
